8b3b28729928b52ae215dc0daa5ace2972012236
Karl Matthes <karlmatthes@gmail.com>
2019-11-05 20:12:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,7 +15,11 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  nil
+  director_gross = 0
+  director_data[:movie].length.times do |cycle_titles|
+    director_gross += director_data[:movie][cycle_titles][:worldwide_gross]
+  end
+  return director_gross
 end
 
 
